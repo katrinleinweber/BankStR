84092d61418aaed1af73cc0ceee596dde278e4e9
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-01-06 14:25:49 +0100

    test Consors data

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -3,6 +3,7 @@ context("Data bundled with BankStR package")
 test_that("Variable names of the imported dataframes: ", {
   expect_equal(
     names(read_gls("../../data/gls_Umsaetze_DDMMYYYY_bis_DDMMYYYY_KTO1234567890_DDMMYYYY.csv")),
+    names(read_consors("../../data/consors_Buchungen_200845694.csv")),
     names(read_sparda("../../data/sparda_umsaetze-1234567-YYYY-MM-DD-HH-MM-SS.csv")),
     names(read_sparkasse("../../data/sparkasse_YYYYMMDD-12345678-umsatz.CSV")),
     names(read_wustenrot("../../data/wustenrot_1234567890_YYYYMMDD_YYYYMMDD.csv")),
