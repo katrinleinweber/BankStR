ca6d46bf0451d7a2bf2c9f515b441bdc683c89f4
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-01-06 14:26:43 +0100

    test DATE conversion

--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -11,3 +11,13 @@ test_that("Variable names of the imported dataframes: ", {
   )
 }
 )
+
+test_that("DATE is not NA due to conversion problem: ", {
+  expect_false(is.na(read_gls("../../data/gls_Umsaetze_DDMMYYYY_bis_DDMMYYYY_KTO1234567890_DDMMYYYY.csv")[1,1]))
+  expect_false(is.na(read_consors("../../data/consors_Buchungen_200845694.csv")[1,1]))
+  expect_false(is.na(read_sparda("../../data/sparda_umsaetze-1234567-YYYY-MM-DD-HH-MM-SS.csv")[1,1]))
+  expect_false(is.na(read_sparkasse("../../data/sparkasse_YYYYMMDD-12345678-umsatz.CSV")[1,1]))
+  expect_false(is.na(read_wustenrot("../../data/wustenrot_1234567890_YYYYMMDD_YYYYMMDD.csv")[1,1])
+  )
+}
+)
