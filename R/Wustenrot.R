0d45b83d934fd40aef728a451a7f64c0ce7b79a4
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-01-08 11:18:37 +0100

    retest & repair Wüstenrot import
    initial wustenrot…csv didn’t contain trailing ; for data rows, because it was removed by LibreOffice which I used to anonymise the data

--- R/Wustenrot.R
+++ R/Wustenrot.R
@@ -8,8 +8,16 @@
 #' read_wustenrot('data/wustenrot_1234567890_YYYYMMDD_YYYYMMDD.csv')
 
 read_wustenrot <- function(file) {
-    df <- read.csv2(file = file)
+    df <- read.csv2(file, header = FALSE, skip = 1)
     
+    # remove empty last column
+    df <- df[, -ncol(df)]
+    
+    # prepare header seperately, because it does not end with ;
+    # data rows have trailing ; => error in read.table
+    header <- readLines(file, n = 1)
+    names(df) <- unlist(strsplit(header, ";"))
+
     df <- clean_bank_df(df, variables = c("Wertstellung", "Buchungsinformationen", "Betrag"), dateformat = "%d.%m.%y")
     
     return(df)
