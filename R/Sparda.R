a9101be87ce6f8c9ae51de797af02562cc696315
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-03-18 19:38:41 +0100

    Revert 34addc0a "removing invalid CSVs…" & attempting to limit reading invalid CSVs resurges…
    Error in read.table(zfile, header = TRUE, sep = ";", as.is = FALSE) :
      more columns than column names

--- R/Sparda.R
+++ R/Sparda.R
@@ -9,10 +9,19 @@
 #' @return A dataframe reduced to the variables 'DATE', 'DESCRIPTION' and 'AMOUNT' of
 #' the transactions.
 #'
+#' @examples
+#' read_sparda('data/sparda_umsaetze-1234567-YYYY-MM-DD-HH-MM-SS')
+#'
 #' @export
 
 read_sparda <- function(file) {
-    df <- readr::read_csv2(file = file, skip = 10)
+    df <- readr::read_csv2(file = file, skip = 10,
+                           col_types = cols_only(Buchungstag = col_guess(),
+                                                 Wertstellungstag = col_guess(),
+                                                 Verwendungszweck = col_character(),
+                                                 Umsatz = col_number()
+                                                 )
+                           )
 
     # Remove last row with 'noch nicht ausgef?hrte Ums?tze'; learned from
     # http://r.789695.n4.nabble.com/remove-last-row-of-a-data-frame-td4652858.html
--- R/Wustenrot.R
+++ R/Wustenrot.R
@@ -5,11 +5,20 @@
 #' @return A dataframe reduced to the variables 'DATE', 'DESCRIPTION' and 'AMOUNT' of
 #' the transactions.
 #'
+#' @examples
+#' read_wustenrot('data/wustenrot_1234567890_YYYYMMDD_YYYYMMDD.csv')
+#'
 #' @export
 
 read_wustenrot <- function(file) {
 
-    df <- readr::read_csv2(file)
+    df <- readr::read_csv2(file, col_types = cols_only(Datum = col_character(),
+                                                       Wertstellung = col_character(),
+                                                       Vorgangsart = col_character(),
+                                                       Buchungsinformationen = col_character(),
+                                                       Betrag = col_number()
+                                                       )
+                           )
 
 
     df <- clean_bank_df(df, variables = c("Wertstellung", "Buchungsinformationen", "Betrag"),
